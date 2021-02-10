5e45f653d04ac52f68978cd8a931c7dda294d47a
Surya Thakur <77577564+Surya0705@users.noreply.github.com>
2021-02-10 20:47:03 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,7 @@
+import math # Importing the Math Module.
+
+a = int(input("Enter the Number: ")) # Taking the Number as Input from User.
+
+'''In the Line-7 I have used the sqrt function from the module for finding the Square
+ Root. I have even Used f string to Print results effectively.'''
+print(f"The Square Root of {a} is {math.sqrt(a)}.") # Printing the Output.
